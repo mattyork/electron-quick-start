1cd589daaacf494271ba27c8c8831aa26460969e
Matt York <york.matt@gmail.com>
2019-02-25 13:34:28 -0800

    update comment

--- orphaned-process.js
+++ orphaned-process.js
@@ -4,7 +4,7 @@ const child_process = require("child_process");
 child_process.fork(
     "node_modules/electron/cli.js",
     ["."],
-    // In Electron v3.x on Windows, when `silent` is true, ctrl-c will frequently orphan renderer processes.
-    // This is not a problem when silent is false, on Electron 2, on Electron 4, or on Mac
+    // In Electron > v3 on Windows, when `silent` is true, ctrl-c will frequently orphan renderer processes.
+    // This is not a problem when silent is false on Electron 2.
     { silent: true }
 );
